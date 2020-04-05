b874d7d02c3bb5d82d5ce4280030283b7726d0d3
vagrant <vagrant@localhost.localdomain>
2020-04-05 05:25:37 +0000

    [Finish] 5-9 (課題3)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'homes#top'
-  get 'homes/about' => 'home#about'
+  root 'home#top'
+  get 'home/about' => 'home#about'
   resources :users, only: [:show, :edit, :index, :update]
   resources :books, only: [:index,:show, :create, :edit, :update, :destroy]
 end
